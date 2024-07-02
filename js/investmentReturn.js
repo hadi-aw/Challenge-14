a6c157d0d532d07c44289bae2cee5962724b118e
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-07-01 20:57:22 -0400

    investmentReturn.js
    "Develop investment return calculation module."

---
+++ js/investmentReturn.js
@@ -0,0 +1,14 @@
+// investmentReturn.js
+
+/**
+ * Calculate the future value of an investment.
+ * @param {number} principle - The initial amount of money.
+ * @param {number} rate - The annual interest rate (as a decimal, e.g. 0.05 for 5%).
+ * @param {number} time - The number of years the money is invested for.
+ * @returns {number} The future value of the investment.
+ */
+
+export function calculateInvestmentReturn(principle, rate, time) {
+    const ratePerPeriod = rate / 100;
+    return principle * Math.pow((1 + ratePerPeriod), time);
+}
